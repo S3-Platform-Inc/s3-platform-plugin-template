dbdccc22662e4d88b21a05610196867460a091e9
CuberHuber <lupashcko.rom@yandex.ru>
2024-07-14 01:19:11 +0300

    The S3 Platform Plugin. Next Generation. S3Plugin 3.0

--- src/s3_platform_plugin_template/config.py
+++ src/s3_platform_plugin_template/config.py
@@ -11,20 +11,20 @@ from s3p_sdk.plugin.config import (
 )
 from s3p_sdk.plugin.config.type import SOURCE
 from s3p_sdk.module import (
-    WEBDRIVER,
+    WebDriver,
 )
 
 config = PluginConfig(
     plugin=CoreConfig(
-        reference='uniq source name',
+        reference='my-template-source',
         type=SOURCE,
-        files=['payload_parser.py', ],
+        files=['template_payload.py', ],
         is_localstorage=False
     ),
     task=TaskConfig(
         trigger=trigger.TriggerConfig(
             type=trigger.SCHEDULE,
-            interval=datetime.timedelta(days=7),
+            interval=datetime.timedelta(days=1),
         )
     ),
     middleware=MiddlewareConfig(
@@ -32,16 +32,18 @@ config = PluginConfig(
             modules.TimezoneSafeControlConfig(order=1, is_critical=True),
             modules.CutJunkCharactersFromDocumentTextConfig(order=2, is_critical=True,
                                                             p_fields=['text', 'abstract']),
+            modules.FilterOnlyNewDocumentWithDB(order=3, is_critical=True),
+            modules.SaveDocument(order=4, is_critical=True),
         ],
         bus=None,
     ),
     payload=payload.PayloadConfig(
-        file='payload_parser.py',
-        classname='MyParser',
+        file='template_payload.py',
+        classname='MyTemplateParser',
         entry=payload.entry.EntryConfig(
             method='content',
             params=[
-                payload.entry.ModuleParamConfig(key='driver', module_name=WEBDRIVER, bus=True),
+                payload.entry.ModuleParamConfig(key='driver', module_name=WebDriver, bus=True),
                 payload.entry.ConstParamConfig(key='max_count_documents', value=50),
                 payload.entry.ConstParamConfig(key='url',
                                                value='url to the source page'),
